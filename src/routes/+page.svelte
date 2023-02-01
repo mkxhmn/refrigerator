b23131623802fd77143082550073ee218e48d87f
mkxhmn <fnikman@gmail.com>
2023-02-01 13:41:42 +0800

    feat: tailwind ftw!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex justify-center content-center p-2 flex-col">
+	<h1 class="text-3xl font-bold mb-2">Hello world!</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.neutral.100);
+		padding: theme(padding.2);
+	}
+</style>
