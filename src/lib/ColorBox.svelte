9636e280f47462c263158aa82dd951fcd0f9148b
mkxhmn <fnikman@gmail.com>
2023-02-07 23:06:17 +0800

    feat: ColorBox component

---
+++ src/lib/ColorBox.svelte
@@ -0,0 +1,20 @@
+<script context="module" lang="ts">
+	export type ColorProps =
+		| 'black'
+		| 'red'
+		| 'green'
+		| 'yellow'
+		| 'blue'
+		| 'magenta'
+		| 'cyan'
+		| 'white';
+</script>
+
+<script lang="ts">
+	export let color: ColorProps;
+</script>
+
+<span
+	class="h-16 w-16 rounded-md col-span-1 border-black border-2"
+	style="background-color:{color};"
+/>
