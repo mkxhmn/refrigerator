6004af6f02c3e15121c8005b3e09f057e767edf1
mkxhmn <fnikman@gmail.com>
2023-02-07 23:07:15 +0800

    feat: implement ColorBox components
    maybe I overthink about the implementation for input, this
    implementation leverages on svelte reactivity. I like this one better as
    it is simpler compared to FormData approach

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,62 @@
 <script lang="ts">
-	import Validation from '$lib/Validation.svelte';
-	import Guess from '../lib/Guess.svelte';
+	import Validation from '$lib/Guess.Validation.svelte';
+	import Guess from '$lib/Guess.svelte';
+	import type { ColorProps } from '$lib/ColorBox.svelte';
+	import ColorBox from '$lib/ColorBox.svelte';
 
-	const codePegs = ['magenta', 'orange', 'blue', 'purple', 'yellow', 'white', 'green', 'black'];
+	const codePegs: ColorProps[] = [
+		'black',
+		'red',
+		'green',
+		'yellow',
+		'blue',
+		'magenta',
+		'cyan',
+		'white'
+	];
 
-	const inputs = ['first', 'second', 'third', 'fourth'];
+	let inputs: ColorProps[] = ['magenta', 'magenta', 'magenta', 'magenta'];
 
-	const secret = ['magenta', 'yellow', 'purple', 'black'];
-	let guesses: string[][] = [];
+	const secret: ColorProps[] = ['magenta', 'yellow', 'cyan', 'black'];
+	let guesses: ColorProps[][] = [];
 
-	const handleSubmit = (e: Event) => {
-		if (e.target instanceof HTMLFormElement) {
-			const form = new FormData(e.target);
-
-			guesses.push(form.getAll('selection') as string[]);
-
-			// currently i am not sure if this is a best way to handle reactivity for array
-			guesses = guesses;
+	const handleSelect = (e: Event, index: number) => {
+		if (e.target instanceof HTMLSelectElement) {
+			inputs[index] = e.target.value as ColorProps;
 		}
+	};
+
+	const handleSubmit = () => {
+		// perform a shallow copy to guesses
+		guesses.push(inputs.slice());
+		guesses = guesses;
 	};
 </script>
 
 <div id="container">
 	<div class="grid grid-cols-2 m-8 mt-4 h-full rounded-xl overflow-hidden border-black border-2">
 		<div class="bg-red-50 px-8 flex flex-col content-center justify-center">
-			<form on:submit|preventDefault={handleSubmit}>
-				<div class="grid grid-cols-4 gap-4">
-					{#each inputs as input}
-						<select id={input} name="selection">
+			<div class="grid grid-cols-4 gap-4">
+				{#each inputs as input, index}
+					<div class="grid grid-rows-2 gap-2">
+						<ColorBox color={input} />
+						<select
+							id={input}
+							value={input}
+							on:change={(e) => handleSelect(e, index)}
+							name="selection"
+						>
 							{#each codePegs as peg}
 								<option value={peg}>{peg}</option>
 							{/each}
 						</select>
-					{/each}
-				</div>
-				<button
-					class="py-4 w-full border-b-8 transition-all ease-out duration-200 hover:border-b-2 text-center mt-6 bg-neutral-300 rounded-full uppercase text-lg font-bold border-2 border-black"
-					type="submit">submit</button
-				>
-			</form>
+					</div>
+				{/each}
+			</div>
+			<button
+				class="py-4 w-full border-b-8 transition-all ease-out duration-200 hover:border-b-2 text-center mt-6 bg-neutral-300 rounded-full uppercase text-lg font-bold border-2 border-black"
+				on:click={handleSubmit}>submit</button
+			>
 		</div>
 		<div class="bg-purple-50 p-8 overflow-y-scroll rounded-tr-xl rounded-br-xl">
 			{#each guesses as guess, index (index)}
