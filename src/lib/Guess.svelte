8fa90b2902e7396db2a355610ebc09f6560bf4fb
mkxhmn <fnikman@gmail.com>
2023-02-07 23:06:56 +0800

    feat: implement ColorBox

--- src/lib/Guess.svelte
+++ src/lib/Guess.svelte
@@ -1,14 +1,13 @@
 <script lang="ts">
-	export let guess: string[] = [];
+	import ColorBox, { type ColorProps } from './ColorBox.svelte';
+
+	export let guess: ColorProps[] = [];
 </script>
 
 <div class="grid grid-cols-4 my-4 gap-4 justify-center">
 	{#each guess as color}
 		<div class="flex justify-center">
-			<span
-				class="h-16 w-16 rounded-md col-span-1 border-black border-2"
-				style="background-color:{color};"
-			/>
+			<ColorBox {color} />
 		</div>
 	{/each}
 </div>
